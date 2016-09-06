48642fed0397cb460e5290381462e643d0612c6c
Jorge Mero <Jorge Mero@192.168.0.3>
2016-09-05 23:35:00 -0500

    3 commit

--- Rubrica32 ing/src/rubrica32/ing/Rubrica32Ing.java
+++ Rubrica32 ing/src/rubrica32/ing/Rubrica32Ing.java
@@ -5,6 +5,8 @@
  */
 package rubrica32.ing;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Jorge Mero
@@ -15,9 +17,22 @@ public class Rubrica32Ing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Bienvendio a Java");
-        System.out.println("Es fàcil");
+       
+     int valor1=0;//Deglaro mi variable para mi primer valor // int significa que mi variable es entera
+     int valor2=0;//Declaro mi variable para mi segundo valor 
+     int suma=0;//declaro la variable de mi suma ; esta acumula la operaciòn de mi suma con valor1 + valor2
+    Scanner entrada= new Scanner(System.in);//En la variable entrada puedo colocar otras variable como; mientrada, ingreso, teclado etc..
+    System.out.println("Ingrese el primer valor =>");
+    valor1=entrada.nextInt();
+    System.out.println("Ingrese el segundo valor =>");
+    valor2=entrada.nextInt();
+    suma=valor1+valor2;
+    // System.out.printf("La suma %d \n",suma); // f es para mostrar algo pero en formato
+    System.out.printf("La suma %d más %d es igual ===> %d\n",valor1,valor2,suma);// puedo mostra mi mensaje de esta manera o tambien como de la forma anterior
+                                                                                // Este %d es como significara valor1 y el otro %d es valor2 y de ultimo la palabra sera la suma
+                                                                               // con esta barra puedo escribir comentario que me ayuden a recordar lo que se realizo en cada programa 
+                                                                              // es decir que estos comentarios no se van a ejecutar (ni mostrar) en mi programa  
+   }
     }
     
 }

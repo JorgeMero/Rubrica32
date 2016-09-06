c74359dc02ce4f9d20349fe38dc46b35ccc0c468
Jorge Mero <Jorge Mero@192.168.0.3>
2016-09-05 23:23:31 -0500

    1 commit

---
+++ Rubrica32 ing/src/rubrica32/ing/Rubrica32Ing.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rubrica32.ing;
+
+/**
+ *
+ * @author Jorge Mero
+ */
+public class Rubrica32Ing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bienvendio a Java");
+    }
+    
+}
